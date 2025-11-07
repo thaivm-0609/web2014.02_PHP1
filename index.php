004cc8ee47baa93ba67311cc41aec59081242703
thaivm-0609 <vuongthai95@gmail.com>
2025-11-07 07:38:49 +0700

    feat: on tap PHP

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+// (forward slash): cmt cho 1 dòng \\(backward slash)
+/**
+ * comment 
+ * cho
+ * nhiều 
+ * dòng
+ */
+
+/** variable: biến số => giá trị có thể thay đổi
+ * $tenBien: tên biến không được bắt đầu bằng số/ký tự đặc biệt
+ * tenBien: phải có ý nghĩa và gợi nhớ
+ * Quy tắc đặt tên: danh sach sinh vien
+ * - camelCase: danhSachSinhVien => đặt tên biến - hàm
+ * - snake_case: danh_sach_sinh_vien
+ * - PascalCase: DanhSachSinhVien => đặt tên class
+ */
+
+$a = 5; //không gán giá trị
+$b = 10; //khai báo biến và gán giá trị luôn cho nó
+
+/** constant: hằng số => giá trị không thay đổi
+ * const tenHangSo = value; Hằng số bắt buộc phải gán giá trị ngay khi khởi tạo
+ * tên hằng số thường được viết dưới dạng UPPERCASE: TEN_HANG_SO 
+ */
+const test = 1;
+
+echo $a + $b;
+$c = 15;
+$d = 20;
+echo $c + $d;
+
+/** function: hàm => tập hợp xử lý logic (chức năng) để tái sử dụng nhiều lần
+ * function tenHam(param1, param2) {
+ *      code logic
+ *      return
+ * }
+ * 
+ * - Phân loại hàm
+ *  + Dựa vào tham số truyền vào: Có tham số | Không có tham số
+ *  + Dựa vào kết quả trả về: Có return | Không có return
+ * 
+ * - Truyền tham số khi gọi hàm:
+ *  + truyền đủ số lượng tham số như đã khai báo
+ *  + TRUYỀN ĐÚNG THỨ TỰ THAM SỐ
+ */
+function tinhTong($a,$b) { //khai báo (khởi tạo hàm)
+    echo $a + $b;
+}
+tinhTong(10,30);//gọi hàm (thực thi hàm)
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Danh sach</h1>
+</body>
+</html>
