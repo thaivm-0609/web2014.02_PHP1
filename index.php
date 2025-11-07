4d28d1ffeec422f7d9ac59def9eca4b72c2602d8
thaivm-0609 <vuongthai95@gmail.com>
2025-11-07 09:11:24 +0700

    feat: switch case

--- index.php
+++ index.php
@@ -25,10 +25,10 @@ $b = 10; //khai báo biến và gán giá trị luôn cho nó
  */
 const test = 1;
 
-echo $a + $b;
-$c = 15;
-$d = 20;
-echo $c + $d;
+// echo $a + $b;
+// $c = 15;
+// $d = 20;
+// echo $c + $d;
 
 /** function: hàm => tập hợp xử lý logic (chức năng) để tái sử dụng nhiều lần
  * function tenHam(param1, param2) {
@@ -47,20 +47,34 @@ echo $c + $d;
 function tinhTong($a,$b) { //khai báo (khởi tạo hàm)
     echo $a + $b;
 }
-tinhTong(10,30);//gọi hàm (thực thi hàm)
+// tinhTong(10,30);//gọi hàm (thực thi hàm)
 
+function giaiPTB2($a,$b,$c) {
+    $delta = $b*$b - 4*$a*$c;
+    if ($delta < 0) {
+        echo "PT vô nghiệm";
+    } else if ($delta == 0) {
+        $x = -$b/(2*$a);
+        echo "PT có nghiệm kép x1=x2=".$x;
+    } else {
+        $x1 = (-$b + sqrt($delta))/(2*$a);
+        $x2 = (-$b - sqrt($delta))/(2*$a);
+        echo "PT có 2 nghiệm x1=".$x1." và x2=".$x2;
+    }
+}
 
+function checkEvenOdd($number) {
+    // if ($number%2 == 0) {
+    //     echo $number." là số chẵn";
+    // } else {
+    //     echo $number." là số lẻ";
+    // }
+
+    //toán tử 3 ngôi: (đk) ? value nếu đk đúng : value nếu đk sai
+    $result = $number%2 == 0 ? "chẵn" : "lẻ";
+    echo $number." là số ".$result;
+}
+checkEvenOdd(5);
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Danh sach</h1>
-</body>
-</html>
---
+++ switch.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <div>
+            <label for="">Số a</label>
+            <input type="number" name="a">
+        </div>
+        <div>
+            <label for="">Số b</label>
+            <input type="number" name="b">
+        </div>
+        <div>
+            <label for="">Phép tính</label>
+            <select name="operator" id="">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">x</option>
+                <option value="/">/</option>
+            </select>
+        </div>
+        <button type="submit">Tính</button>
+    </form>
+</body>
+</html>
